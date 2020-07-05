5944523427fb584b8d9251e7fbcdc8a070902fb3
unknown <you@example.com>
2020-07-06 01:25:58 +0600

    added circule image view lib

--- app/src/main/java/kg/azimus/instagrammer/ui/profile/ProfileFragment.kt
+++ app/src/main/java/kg/azimus/instagrammer/ui/profile/ProfileFragment.kt
@@ -1,15 +1,19 @@
 package kg.azimus.instagrammer.ui.profile
 
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kg.azimus.instagrammer.R
 import kg.azimus.instagrammer.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
+
 
 class ProfileFragment : BaseFragment() {
+
     override fun getLayoutId(): Int {
         return R.layout.fragment_profile
     }
 
     override fun onViewReady(view: View) {
-
+        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
     }
 }
